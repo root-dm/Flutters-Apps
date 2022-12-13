fdded7742e8dbf11c00be59ad48b810d485d17b5
root-dm <proangel99@gmail.com>
2022-12-13 14:20:19 +0200

    remove unc

--- assigm2/android/app/src/main/kotlin/com/example/assigm2/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.assigm2
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- assss/ass/android/app/src/main/kotlin/com/example/ass/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.ass
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
